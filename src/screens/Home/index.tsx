c9e8b7647a7fdcceb6db869b1625dec53e542ee7
Luan <luanrodriguesdev@gmail.com>
2022-09-15 18:47:45 -0300

    created navigator tab

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,39 +1,42 @@
-import { View, Image, FlatList } from 'react-native';
+import { useEffect, useState } from "react";
+import { Image, FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import logoImg from '../../assets/logo-nlw-esports.png'
-import { GameCard } from '../../components/GameCard';
-import { Heading } from '../../components/Heading';
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { GameCard, GameCardProps } from "../../components/GameCard";
+import { Heading } from "../../components/Heading";
 
-import { GAMES } from '../../utils/games'
-
-import { styles } from './styles';
+import { styles } from "./styles";
 
 export function Home() {
-  return (
-    <View style={styles.container}>
-      <Image 
-        source={logoImg} 
-        style={styles.logo}
-      />
+  const [games, setGames] = useState<GameCardProps[]>([]);
 
-      <Heading 
-        title='Encontre seu duo!'
-        subtitle='Selecione o game que deseja jogar...'
+  useEffect(() => {
+    fetch("http://192.168.15.17:3333/games")
+      .then((response) => response.json())
+      .then((data) => {
+        setGames(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
       />
 
       <FlatList
-        data={GAMES}
-        keyExtractor={item => item.id}
-        renderItem={({item}) => (
-          <GameCard 
-            data={item}
-          />
-        )}
+        data={games}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
         showsHorizontalScrollIndicator={false}
         horizontal
         contentContainerStyle={styles.contentList}
       />
-
-    </View>
+    </SafeAreaView>
   );
 }
